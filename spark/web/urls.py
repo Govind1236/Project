695881c728b90e672af94b782189a1bffcc88a7a
Govind Basnet <govindbasnet02@gmail.com>
2024-12-09 22:15:47 +0545

    Feat: add view

--- spark/web/urls.py
+++ spark/web/urls.py
@@ -2,5 +2,7 @@ from.import views
 from django.urls import path
 
 urlpatterns = [
-    path('',views.index,name='index')
+    path('index/',views.index,name='index'),
+    path('about/',views.about,name='about'),
+
 ]
--- spark/web/views.py
+++ spark/web/views.py
@@ -3,3 +3,5 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('Hello its working')
+def about(request):
+    return HttpResponse('This is about page')
