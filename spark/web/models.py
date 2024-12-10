fda3eb67cea9c58790ff8d810cb4f0acb4d3088b
Govind Basnet <govindbasnet02@gmail.com>
2024-12-10 21:11:26 +0545

    feat: add data in model

--- spark/web/models.py
+++ spark/web/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Person(models.Model):
-    person_name = models.CharField( max_length= 100),
-    person_age = models.IntegerField(),
-    person_num = models.IntegerField()
+    def __str__(self):
+        return self.person_name
+    
+    person_name = models.CharField( max_length= 100)
+    person_age = models.IntegerField(default=25)
+    person_num = models.IntegerField() 
