2ae33f4b83312d83ac296f3bbc99f143c4790b09
Govind Basnet <govindbasnet02@gmail.com>
2024-12-10 20:25:41 +0545

    Feat: add models in Db

--- spark/web/models.py
+++ spark/web/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    person_name = models.CharField( max_length= 100),
+    person_age = models.IntegerField(),
+    person_num = models.IntegerField()
