21094293aab7f25edd0dc8ccffce75720523460b
Govind Basnet <govindbasnet02@gmail.com>
2024-12-11 21:35:11 +0545

    inset variable in model.py

--- spark/web/models.py
+++ spark/web/models.py
@@ -2,9 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Person(models.Model):
-    def __str__(self):
-        return self.person_name
+    # def __str__(self):
+    #     return self.person_name
     
     person_name = models.CharField( max_length= 100)
     person_age = models.IntegerField(default=25)
     person_num = models.IntegerField() 
+    person_address = models.TextField(null= True, blank = False)
