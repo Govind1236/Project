a456fa51f3476fc369c93b3edb5f9cc5c6105741
Govind Basnet <govindbasnet02@gmail.com>
2024-12-12 11:33:39 +0545

    change in urls.py

--- spark/web/urls.py
+++ spark/web/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('home/',views.home,name='home'),
     path('about/',views.about,name='about'),
     path('contact/',views.contact,name='contact'),
-    path('',views.Index,name='index'),
+    path('index/',views.Index,name='index'),
     #/home/1
     path('<int:person_id>/',views.detail,name='detail'),
     
