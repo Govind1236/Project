731d1cefffb49adf5e2fe9c7a50a67eda03f7d41
Govind Basnet <govindbasnet02@gmail.com>
2024-12-13 20:30:57 +0545

    static file

--- spark/web/views.py
+++ spark/web/views.py
@@ -4,20 +4,15 @@ from.models import Person
 from django.template import loader
 # Create your views here.
 def home(request):
-    list_result = Person.objects.all() #extracting data from database 
-    template = loader.get_template('Sparks/index.html')
+    list_result = Person.objects.all() #extract data from database
     context = {
-        'list_result': list_result,
-    }
-    return HttpResponse (template.render(context,request))  #returning data view to home url /
-def about(request):
-    list_result = Person.objects.all()
     # temp = loader.get_template('Sparks/about.html')
-    context = {
        'list_result': list_result,
     }
     # return HttpResponse(temp.render(context,request))
-    return render(request,'Sparks/about.html',context)
+    return render(request,'Sparks/index.html',context) #returning data view to home url /
+def about(request):
+    return render(request,'Sparks/about.html')
 def contact(req):
     return HttpResponse("Its an contact page")
 def Index(req):
